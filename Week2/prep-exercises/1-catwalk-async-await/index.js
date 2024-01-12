d9520b0c94e82af032c2279f58349f643899a4ea
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-01-12 21:03:02 +0100

    W2-Done

--- Week2/prep-exercises/1-catwalk-async-await/index.js
+++ Week2/prep-exercises/1-catwalk-async-await/index.js
@@ -4,27 +4,71 @@ const STEP_INTERVAL_MS = 50;
 const STEP_SIZE_PX = 10;
 const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
-  'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+    'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 
-function walk(img, startPos, stopPos) {
-  return new Promise((resolve) => {
-    // Copy over the implementation from last week
-  });
+function walk(img, startPos, stopPos, isDancing) {
+    return new Promise((resolve) => {
+        // Copy over the implementation from last week
+        let currentPos = startPos;
+
+        const move = () => {
+            if (!isDancing) {
+                currentPos += STEP_SIZE_PX;
+                img.style.left = currentPos + 'px';
+
+                if (currentPos < stopPos) {
+                    setTimeout(move, STEP_INTERVAL_MS);
+                } else {
+                    resolve();
+                }
+            } else {
+                resolve(); //skip
+            }
+        };
+
+        setTimeout(move, 0);
+    });
 }
 
 function dance(img) {
-  return new Promise((resolve) => {
-    // Copy over the implementation from last week
-  });
+    return new Promise((resolve) => {
+        // Copy over the implementation from last week
+        const originalImg = img.src;
+
+        img.src = DANCING_CAT_URL;
+
+        setTimeout(() => {
+            img.src = originalImg;
+            resolve();
+        }, DANCE_TIME_MS);
+    });
 }
 
 async function catWalk() {
-  const img = document.querySelector('img');
-  const startPos = -img.width;
-  const centerPos = (window.innerWidth - img.width) / 2;
-  const stopPos = window.innerWidth;
+    const img = document.querySelector('img');
+    const startPos = -img.width;
+    const centerPos = (window.innerWidth - img.width) / 2;
+    const stopPos = window.innerWidth;
 
-  // Use async/await syntax to loop the walk and dance functions
+    // Use async/await syntax to loop the walk and dance functions
+
+    let isDancing = false;
+
+    const loopFunc = async () => {
+        while (true) {
+            await walk(img, startPos, centerPos, isDancing);
+
+            isDancing = true;
+            await dance(img);
+
+
+            isDancing = false;
+            await walk(img, centerPos, stopPos, isDancing);
+        }
+
+    };
+
+    loopFunc();
 }
 
 window.addEventListener('load', catWalk);
--- Week2/prep-exercises/2-pokemon-fetch/index.js
+++ Week2/prep-exercises/2-pokemon-fetch/index.js
@@ -16,47 +16,64 @@ const VALID_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=5';
 const INVALID_URL = 'https://pokeapi.co/api/v2/pokemons/?limit=5';
 
 async function fetchJSON(url) {
-  // TODO
+    // Fetch the JSON data from the web API that responds to the `url` parameter
+    // and return a promise that resolves to a corresponding JavaScript object.
+    // Make sure to check for HTTP errors.
+    try {
+        const response = await fetch(url);
 
-  // Fetch the JSON data from the web API that responds to the `url` parameter
-  // and return a promise that resolves to a corresponding JavaScript object.
-  // Make sure to check for HTTP errors.
+        if (!response.ok) {
+            throw new Error(`HTTP error!`);
+        }
+
+        return response.json();
+
+    } catch (err) {
+        console.log(`Fetching failed! ${err.message}`);
+    }
 }
 
 function renderResults(pokemons) {
-  // 1. Clear the text content of the HTML element with id `error`.
-  const errorElement = document.querySelector('#error');
-  errorElement.innerText = '';
+    // 1. Clear the text content of the HTML element with id `error`.
+    const errorElement = document.querySelector('#error');
+    errorElement.innerText = '';
 
-  // 2. Set the text content of the HTML element with id `json` to JSON text
-  //    from the `pokemons` argument, formatted in a human readable form (i.e.,
-  //    with indentation and line breaks).
-  const pokemonsElement = document.querySelector('#json');
-  pokemonsElement.innerText = JSON.stringify(pokemons, null, 2);
+    // 2. Set the text content of the HTML element with id `json` to JSON text
+    //    from the `pokemons` argument, formatted in a human readable form (i.e.,
+    //    with indentation and line breaks).
+    const pokemonsElement = document.querySelector('#json');
+    pokemonsElement.innerText = JSON.stringify(pokemons, null, 2);
 }
 
 function renderError(err) {
-  // 1. Clear the text content of the HTML element with id `json`.
-  const pokemonsElement = document.querySelector('#json');
-  pokemonsElement.innerText = '';
+    // 1. Clear the text content of the HTML element with id `json`.
+    const pokemonsElement = document.querySelector('#json');
+    pokemonsElement.innerText = '';
 
-  // 2. Set the text content of the HTML element with id `error` to the
-  //    `.message` property of the `err` parameter.
-  const errorElement = document.querySelector('#error');
-  errorElement.innerText = err;
+    // 2. Set the text content of the HTML element with id `error` to the
+    //    `.message` property of the `err` parameter.
+    const errorElement = document.querySelector('#error');
+    errorElement.innerText = err;
 }
 
 function main() {
-  const button = document.querySelector('#button');
-  button.addEventListener('click', () => {
-    const option = document.querySelector('#option');
-    const url = option.checked ? INVALID_URL : VALID_URL;
+    const button = document.querySelector('#button');
+    button.addEventListener('click', async () => {
+        const option = document.querySelector('#option');
+        const url = option.checked ? INVALID_URL : VALID_URL;
 
-    // TODO
-    // Use `fetchJSON()` to fetch data from the selected url.
-    // If successful, render the data by calling function `renderResults()`.
-    // On failure, render the error by calling function `renderError()`.
-  });
+        // TODO
+        // Use `fetchJSON()` to fetch data from the selected url.
+        // If successful, render the data by calling function `renderResults()`.
+        // On failure, render the error by calling function `renderError()`.
+        try {
+            const data = await fetchJSON(url);
+            renderResults(data);
+        } catch (err) {
+            renderError(err.message);
+        }
+
+    });
 }
 
 window.addEventListener('load', main);
