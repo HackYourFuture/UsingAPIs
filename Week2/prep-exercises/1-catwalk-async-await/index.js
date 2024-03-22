526c11681211aa50c73ad3dc206913140168158d
rasha <rasha_alshraim@hotmail.com>
2024-03-22 18:02:07 +0100

    add week2 prep

--- Week2/prep-exercises/1-catwalk-async-await/index.js
+++ Week2/prep-exercises/1-catwalk-async-await/index.js
@@ -9,13 +9,33 @@ const DANCING_CAT_URL =
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
     // Copy over the implementation from last week
+    let currentPos = startPos;
+
+    const walkStep = () => { 
+      if (currentPos < stopPos) {
+        currentPos += STEP_SIZE_PX;
+        img.style.left = currentPos + 'px';
+        setTimeout(walkStep, STEP_INTERVAL_MS); 
+      } else {
+        resolve();
+      }
+    };
+
+    walkStep();
   });
 }
 
 function dance(img) {
   return new Promise((resolve) => {
     // Copy over the implementation from last week
-  });
+    const walkingCatSrc = img.src;
+    img.src = DANCING_CAT_URL;
+
+    setTimeout(() => {
+      img.src = walkingCatSrc;
+      resolve();
+    }, DANCE_TIME_MS);
+   });
 }
 
 async function catWalk() {
@@ -25,6 +45,11 @@ async function catWalk() {
   const stopPos = window.innerWidth;
 
   // Use async/await syntax to loop the walk and dance functions
+  while (startPos < stopPos) { 
+    await walk(img, startPos, centerPos)
+    await dance(img)
+    await walk(img, centerPos, stopPos)
+  }
 }
 
 window.addEventListener('load', catWalk);
--- Week2/prep-exercises/2-pokemon-fetch/index.js
+++ Week2/prep-exercises/2-pokemon-fetch/index.js
@@ -16,11 +16,21 @@ const VALID_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=5';
 const INVALID_URL = 'https://pokeapi.co/api/v2/pokemons/?limit=5';
 
 async function fetchJSON(url) {
-  // TODO
 
   // Fetch the JSON data from the web API that responds to the `url` parameter
   // and return a promise that resolves to a corresponding JavaScript object.
   // Make sure to check for HTTP errors.
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status}`);  
+    }
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+    console.log(`Failed to fetch: ${error.message}`);
+  }
 }
 
 function renderResults(pokemons) {
@@ -48,14 +58,19 @@ function renderError(err) {
 
 function main() {
   const button = document.querySelector('#button');
-  button.addEventListener('click', () => {
+  button.addEventListener('click', async () => {
     const option = document.querySelector('#option');
     const url = option.checked ? INVALID_URL : VALID_URL;
 
-    // TODO
     // Use `fetchJSON()` to fetch data from the selected url.
     // If successful, render the data by calling function `renderResults()`.
     // On failure, render the error by calling function `renderError()`.
+    try {
+      const data = await fetchJSON(url);
+      renderResults(data);
+    } catch (error) {
+      renderError(error);
+    }
   });
 }
 
